80ba6f285df2c0826dcd7cf2ef08d1515c12a94c
Mr-Cheen1 <sda8989bkru@gmail.ru>
2023-12-29 16:54:43 +0400

    HW03

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	fmt.Println(`Генератор шахматного поля приветствует Вас!`)
+	var rows, cols int
+	fmt.Print(`Введите размер поля в формате 'axb',
+где 'a' - это количество строк,
+а 'b' - количество столбцов: `)
+	fmt.Scanf("%dx%d", &rows, &cols)
+
+	fmt.Print(" ")
+	for a := 0; a < cols; a++ {
+		fmt.Print("---")
+	}
+	fmt.Println()
+
+	for a := 0; a < rows; a++ {
+		fmt.Print("|")
+		for b := 0; b < cols; b++ {
+			if (a+b)%2 == 0 {
+				fmt.Print("   ")
+			} else {
+				fmt.Print(" # ")
+			}
+		}
+		fmt.Println("|")
+	}
+
+	fmt.Print(" ")
+	for a := 0; a < cols; a++ {
+		fmt.Print("---")
+	}
 }
